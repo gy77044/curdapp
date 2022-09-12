380e8d8d7e38388fbdf3ddf563b8b14109b14147
gautam yadav <gy77044@gmail.com>
2022-09-12 11:59:10 +0530

    first
    first commit

---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+  {
+    id:1,
+    name:"Raman Nava",
+    username:'Bret'
+  },
+  {
+    id:2,
+    name:"Raj Rava",
+    username:'Antonette'
+  },
+  {
+    id:3,
+    name:"Rohan Nava",
+    username:'Peter'
+  },
+  {
+    id:4,
+    name:"King Kohli",
+    username:'virat'
+  },
+  {
+    id:5,
+    name:"M S Dhoni",
+    username:'Mahi'
+  },
+  {
+    id:6,
+    name:"Rahul Dravid",
+    username:'Wall'
+  },
+  {
+    id:7,
+    name:"Barret Nava",
+    username:'Bret'
+  },
+  {
+    id:8,
+    name:"K LNava",
+    username:'Rahul'
+  },
+]
